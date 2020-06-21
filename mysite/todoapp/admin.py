b7870b59172568efecca1be8c088f4793d061bd0
spirittq <michejev@gmail.com>
2020-06-21 21:00:58 +0300

    first commit

--- mysite/todoapp/admin.py
+++ mysite/todoapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Task, Step, Category, Profile
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'category', 'date_created', 'due, date', 'status', 'importnace')
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Step)
 admin.site.register(Category)
 admin.site.register(Profile)
